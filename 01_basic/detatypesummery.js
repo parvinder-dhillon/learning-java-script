88c5ac632f6d4ce84b4eb55c1a665063a479ace4
parvinder singh dhillon <parvinder49427@gmail.com>
2025-03-22 12:54:53 +0000

    detatype summery

---
+++ 01_basic/detatypesummery.js
@@ -0,0 +1,38 @@
+//there are two types of detatypes 
+//permitive detatypes and non permitive detatypes refrence
+
+//permitive detatypes
+// there are 7 types of premetive detatypes
+// string, number, symbol, bigint, null, boolean, undefined
+const score = 123 //number
+const loggenin = true //boolean
+let useraddres; //undefined
+
+// const userid = Symbol('123')
+// const user2id = Symbol('123')
+//BOTH ARE DEFRENT because of symbol
+
+const bignumber = 7218947913658932043740n // this is bigint couse of n in the last
+
+//refrence(non permitive detatype)
+// there are 3 types of non premetive detatypes
+
+// Array , Object , Functions
+
+// array
+
+const heros = ["shaktiman", "captain", "ironman", "thor"]
+
+// object
+
+let obj = {
+    name: "inder",
+    age: 22,
+}
+
+// functions
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
