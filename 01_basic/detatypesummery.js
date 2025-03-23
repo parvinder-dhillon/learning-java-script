89fcb6d3e0a2966b2e8f86f01c5f51b82c9cd295
parvinder singh dhillon <parvinder49427@gmail.com>
2025-03-23 08:36:05 +0000

    diffrence between permitive and non permitive detatypes

--- 01_basic/detatypesummery.js
+++ 01_basic/detatypesummery.js
@@ -35,4 +35,37 @@ let obj = {
 const myfunction = function(){
     console.log("Hello world");
 }
+//stack (permitive), Heap (non permitive)
+// in permitive detatypes if you make two variables and give 2nd variable the first one so the value is same in both variable but when you change value of 2nd it doesnt change the orignal one because it gave you a copy of variable value for example
+
+let first = "parvinder"
+let second = first
+console.log(second)
+
+//now change the value
+second = "inder"
+console.log(first)
+console.log(second)
+//the value doesnt change 
+
+// in non permitive detatypes if you make two variables and give 2nd variable the first one so the value is same in both variable and when you change value of 2nd it change the orignal one because it gave you a orignal refrence of variable value for example
+
+
+let user = {
+    name: "parvinder",
+    lastName: "singh",
+}
+
+let user2 = user
+
+user2.name = "inder"
+
+console.log(user2)
+console.log(user)
+
+//the value is changed 
+
+//this is the difrence between permitive and non permitiv detatypes
+
+
 
